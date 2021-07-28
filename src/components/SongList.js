c09a4f74de51c67db6c29379f6fec7b63fb2f769
ThatoMahloko <thato732mahloko@gmail.com>
2021-07-28 12:40:41 +0200

    first commit

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,16 +4,16 @@ import NewSongFom from './NewSongFom';
 
 const SongList = () => {
     const [song, setSong] = useState([
-        { title: '', artist: '', album: '', releaseYear: '', id: 1 }
+        { title: '', artist: '', album: '', releaseYear: '',url:'', id: 1 }
     ]);
     const addSong = (title,artist, album, releaseYear) => {
         setSong([...song, { title, artist,album,releaseYear, id: uuid() }])
     }
     return (
-        <div>
+        <div style={{textAlign:"center"}}>
             <ul className="list-group">
                 {song.map(song => {
-                    return (<li className="list-group-item bg-info" key={song.id}>Song Title: {song.title} ArtistName:{song.artist} Album Title:{song.album} Release Year:{song.releaseYear}</li>)
+                return (<li href={song.url} className="list-group-item bg-info" key={song.id}>Song Title: {song.title}{<br/>} ArtistName: {song.artist}{<br/>} Album Title: {song.album}{<br/>} Release Year: {song.releaseYear}{<br/>}</li>)
                 })}
             </ul>
             <NewSongFom addSong={addSong}/><hr/>
